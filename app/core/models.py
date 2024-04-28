206d7d456df543a45b54fbe8cb0dadaf8ed7680f
Renan <renanjacobcarvalho@hotmail.com>
2024-04-28 17:46:16 -0300

    Superuser and run settings at docker-compose fixed

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+"""
+Database models
 
-# Create your models here.
+credentials
+
+email: renanjc@example.com
+pass: teste1234
+"""
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionManager
+)
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_field):
+        if not email:
+            raise ValueError('User must have an email address...')
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionManager):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, null = True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
